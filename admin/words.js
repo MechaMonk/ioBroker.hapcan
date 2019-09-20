9bafcb88840b5cdf61aafe46bc64a38193f5cd22
Mariusz <mariusz@pluto.solarsystem.lan>
2019-09-20 17:18:56 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'hapcan adapter settings': {
+        'en': 'Adapter settings for hapcan',
+        'de': 'Adaptereinstellungen für hapcan',
+        'ru': 'Настройки адаптера для hapcan',
+        'pt': 'Configurações do adaptador para hapcan',
+        'nl': 'Adapterinstellingen voor hapcan',
+        'fr': "Paramètres d'adaptateur pour hapcan",
+        'it': "Impostazioni dell'adattatore per hapcan",
+        'es': 'Ajustes del adaptador para hapcan',
+        'pl': 'Ustawienia adaptera dla hapcan',
+        'zh-cn': 'hapcan的适配器设置'
+    },
+    'option1': {
+        'en': 'option1',
+        'de': 'option1',
+        'ru': 'option1',
+        'pt': 'option1',
+        'nl': 'option1',
+        'fr': 'option1',
+        'it': 'option1',
+        'es': 'option1',
+        'pl': 'option1',
+        'zh-cn': 'option1'
+    },
+    'option2': {
+        'en': 'option2',
+        'de': 'option2',
+        'ru': 'option2',
+        'pt': 'option2',
+        'nl': 'option2',
+        'fr': 'option2',
+        'it': 'option2',
+        'es': 'option2',
+        'pl': 'option2',
+        'zh-cn': 'option2'
+    }
+};
