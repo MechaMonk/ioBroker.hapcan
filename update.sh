0795a9260a68e63bdcf0d04dbbf10ba06aa799f4
Mariusz N <mn.git@earth.net.pl>
2019-10-19 12:08:02 +0200

    cp1250 for node description

--- update.sh
+++ update.sh
@@ -12,6 +12,7 @@ cp lib/encoder.js ${iobrokerHapcanDir}/lib
 cp lib/listener.js ${iobrokerHapcanDir}/lib
 cp lib/reader.js ${iobrokerHapcanDir}/lib
 cp lib/tools.js ${iobrokerHapcanDir}/lib
+cp lib/windows-1250.js ${iobrokerHapcanDir}/lib
 cp admin/hapcan.png ${iobrokerHapcanDir}/admin
 cp admin/index_m.html ${iobrokerHapcanDir}/admin
 cp admin/words.js ${iobrokerHapcanDir}/admin
